0885d02941b8738286440ecce51bff3aefc49d5a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-03-31 13:23:47 +0300

    Create Rails project

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theitskillboard::Application.config.secret_token = '89cafb6aa0d0812e472ea9e77af4cd1627d6e545aeda42e789795397d1608faa7aed7e540171eb45a9ae14eb2e34fef7077a9175b721daef5f49a619a472b818'
