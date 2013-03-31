adb677301515d624b91b63b0eb507c601b11301f
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-04-01 00:16:27 +0300

    Simplify Employment migrations

--- db/migrate/20130331204355_create_studies.rb
+++ db/migrate/20130331204355_create_studies.rb
@@ -1,8 +1,14 @@
 class CreateStudies < ActiveRecord::Migration
   def change
     create_table :studies do |t|
+      t.string :program
+      t.date :from_date
+      t.date :to_date
 
       t.timestamps
+
+      t.integer :user_id
+      t.integer :university_id
     end
   end
 end
--- db/migrate/20130331204442_add_user_id_and_university_id_to_study.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdAndUniversityIdToStudy < ActiveRecord::Migration
-  def change
-    add_column :studies, :user_id, :integer
-    add_column :studies, :university_id, :integer
-  end
-end
