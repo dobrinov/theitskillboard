a7464532e44b27df329293f036779f70ed126749
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-04-01 00:10:01 +0300

    Remove obsolite Employment migration

--- db/migrate/20130331203623_add_user_id_and_company_id_to_employment.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdAndCompanyIdToEmployment < ActiveRecord::Migration
-  def change
-    add_column :employments, :user_id, :integer
-    add_column :employments, :company_id, :integer
-  end
-end
