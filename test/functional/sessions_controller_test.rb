9fa3742a8fc51d1b57a50756a63e142096fbab87
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-04-06 05:41:59 -0700

    Fix sessions test

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -2,25 +2,28 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
 
+  def setup
+    @user = users(:simple_user)
+  end
+
   test "should render login form" do
     get :new
     assert_response :success
   end
 
   test "should login the user and redirect to root path" do
-    raise User.last.password_digest.inspect
-    post :create, { :email => 'sample.user@example.com', :password => 'qweqwe' }
+    post :create, { :email => @user.email, :password => 'qweqwe' }
     assert_redirected_to root_path
   end
 
   test "shold show validation errors on the login form in the credentials are not valid" do
-    post :create, { :email => 'sample.user@example.com', :password => 'wrong' }
+    post :create, { :email => @user.email, :password => 'wrong' }
     assert_response :success
     assert_template :new
   end
 
   test "should logout the user and go to the root path" do
-    delete :destroy
+    delete :destroy, { :id => @user.id }
     assert_redirected_to root_path
   end
 
