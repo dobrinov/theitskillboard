faf25b8f69c03ad3bf78f905edf11bba81f2f02a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-04-07 03:40:24 -0700

    Add profiles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Theitskillboard::Application.routes.draw do
   resources :users
+  resources :profiles
   resources :sessions, :only => [:new, :create, :destroy]
 
   get ':action' => 'static#:action'
