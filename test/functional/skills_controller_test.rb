8a7b632ed5474ffc365379059b3e911882e57ed8
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-05-08 18:57:15 +0300

    Write some spec todo`s

--- test/functional/skills_controller_test.rb
+++ test/functional/skills_controller_test.rb
@@ -10,15 +10,25 @@ class SkillsControllerTest < ActionController::TestCase
   end
 
   test 'creating a skill' do
+    pending "Implement"
     post :create, { :profile_id => @user.profile.id, :study_id => @study.id, :course_id => @course.id }
 
     assert_response :success
   end
 
   test 'deleting a skill' do
+    pending "Implement"
     delete :destroy, { :profile_id => @user.profile.id, :study_id => @study.id, :course_id => @course.id, :id => @skill.id }
 
     assert_response :success
+  end
+
+  test 'it should not be able to associate the same skill with course twice' do
+    pending "Implement"
+  end
+
+  test 'it should not be possible to associate a skill to a course not belonging to the current user' do
+    pending "Implement"
   end
 
 end
