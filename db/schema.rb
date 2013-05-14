7de2656743f81a7e7948488ab64126c5a56f8ca2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-05-14 22:13:58 +0300

    Add new schema and update fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(:version => 20130419124520) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
+    t.string   "website"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
