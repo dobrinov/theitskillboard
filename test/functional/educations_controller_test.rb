c5c9ace5d4724af2a7aeabecc34090d5553c25d7
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-05-14 16:57:47 +0300

    Fix problems introduced by the instance variable removal and add test for it

--- test/functional/educations_controller_test.rb
+++ test/functional/educations_controller_test.rb
@@ -3,8 +3,9 @@ require 'test_helper'
 class EducationsControllerTest < ActionController::TestCase
 
   def setup
-    @user  = users(:simple_user)
-    @study = studies(:simple_user_study)
+    @user   = users(:simple_user)
+    @study  = studies(:simple_user_study)
+    @course = courses(:simple_user_study_course)
   end
 
   test 'rendering index view' do
@@ -29,6 +30,16 @@ class EducationsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:study)
     assert_not_nil assigns(:course)
+  end
+
+  test 'rendering new skill form' do
+    login_as @user
+
+    get :index, { :profile_id => @user.profile.id, :new => 'skill', :target => @course.id }
+    assert_response :success
+    assert_not_nil assigns(:study)
+    assert_not_nil assigns(:course)
+    assert_not_nil assigns(:skill)
   end
 
   test 'require login' do
