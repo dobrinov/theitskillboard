cd436f3ed1afa4e8c9e62098929996a2dc94a04a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-06-18 10:42:39 +0300

    Implement complex form logic for employments

--- app/helpers/profile/employment_helper.rb
+++ app/helpers/profile/employment_helper.rb
@@ -4,4 +4,22 @@ module Profile::EmploymentHelper
     params[:new].present? && params[:new] == 'employment'
   end
 
+  def show_new_project_form_for?(target_id, target_type)
+    [
+      params[:new].present?,
+      params[:new] == 'project',
+      target_id.to_s == params[:target_id],
+      target_type == params[:target_type]
+    ].all?
+  end
+
+  def show_new_impact_form_for?(target_id, target_type)
+    [
+      params[:new].present?,
+      params[:new] == 'impact',
+      target_id.to_s == params[:target_id],
+      target_type == params[:target_type]
+    ].all?
+  end
+
 end
