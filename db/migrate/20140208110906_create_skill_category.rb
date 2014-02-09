c690d887d3bd361f705fb5afe31a9e131c22502b
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-02-09 16:32:02 +0200

    Add skill and skill category models

---
+++ db/migrate/20140208110906_create_skill_category.rb
@@ -0,0 +1,10 @@
+class CreateSkillCategory < ActiveRecord::Migration
+  def change
+    create_table :skill_categories do |t|
+      t.string :name
+      t.integer :parent_skill_category_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20140208111124_create_skill.rb
@@ -0,0 +1,9 @@
+class CreateSkill < ActiveRecord::Migration
+  def change
+    create_table :skills do |t|
+      t.string :name
+      t.integer :skill_category_id
+      t.timestamps      
+    end
+  end
+end
