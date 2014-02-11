0cc15953f28abd95ba4816f92c3ab6560b5da57e
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-02-11 02:13:59 +0200

    Add has many skills association in the SkillCategory model

--- app/models/skill_category.rb
+++ app/models/skill_category.rb
@@ -2,4 +2,5 @@ class SkillCategory < ActiveRecord::Base
   belongs_to :user
   belongs_to :parent_category, class_name: "SkillCategory", foreign_key: 'parent_skill_category_id'
   has_many   :sub_categories,  class_name: "SkillCategory", foreign_key: 'parent_skill_category_id' , dependent: :destroy
+  has_many   :skills, dependent: :destroy
 end
