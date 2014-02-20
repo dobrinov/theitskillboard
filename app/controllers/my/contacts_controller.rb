d844bbe998dae2a12bfd34a3d0218e03925aadbe
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-02-20 18:59:59 +0200

    Implement Interest CRUD

---
+++ app/controllers/my/contacts_controller.rb
@@ -0,0 +1,18 @@
+class My::ContactsController < My::CommonController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/controllers/my/interests_controller.rb
@@ -0,0 +1,51 @@
+class My::InterestsController < My::CommonController
+
+  def new
+    @interest = Interest.new
+  end
+
+  def create
+    current_user.interests.build(interest_params)
+
+    if current_user.save
+      redirect_to back_or_default(edit_my_profile_path), notice: "Interest created."
+    else
+      flash.now[:error] = "Interest not created."
+      render :new
+    end
+  end
+
+  def edit
+    @interest = current_user.interests.find(params[:id])
+  end
+
+  def update
+    @interest = current_user.interests.find(params[:id])
+
+    if @interest.update_attributes(interest_params)
+      redirect_to back_or_default(edit_my_profile_path), notice: "Interest updated."
+    else
+      flash.now[:error] = "Interest not updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    @interest = current_user.interests.find(params[:id])
+
+    if @interest.destroy
+      flash[:notice] = "Interest deleted."
+    else
+      flash[:error] = "Interest not deleted."
+    end
+
+    redirect_to back_or_default(edit_my_profile_path)
+  end
+
+  private
+
+  def interest_params
+    params.require(:interest).permit(:name)
+  end
+
+end
