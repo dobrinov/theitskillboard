85cfbd8a18f48e502c5b46de8a1b98493615a133
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-02-20 20:19:10 +0200

    Add database seed on new user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  before_save :seed_initial_skill_tree, if: :new_record?
+
   # Associations
   has_many :skill_categories, dependent: :destroy
   has_many :skills, dependent: :destroy
@@ -35,5 +37,35 @@ class User < ActiveRecord::Base
                     styles: { small: "100x100#", thumb: "50x50#" },
                     default_url: 'placeholders/profile_picture/:style.png'
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def seed_initial_skill_tree
+    # Root skill categories
+    it_skills_category       = self.skill_categories.build({name: 'IT skills'})
+    language_skills_category = self.skill_categories.build({name: 'Language skills'})
+
+    # Sub skill categories
+    programming_category = self.skill_categories.build({name: 'Programming'})
+    frameworks_category  = self.skill_categories.build({name: 'Framewors'})
+    db_category          = self.skill_categories.build({name: 'Databases'})
+    os_category          = self.skill_categories.build({name: 'Operating systems'})
+
+    it_skills_category.sub_categories << programming_category
+    it_skills_category.sub_categories << frameworks_category
+    it_skills_category.sub_categories << db_category
+    it_skills_category.sub_categories << os_category
+
+    english_category = self.skill_categories.build({name: 'English'})
+
+    language_skills_category.sub_categories << english_category
+
+    # Skills
+    english_speaking_skill      = self.skills.build({name: 'Speaking', level: 5})
+    english_writting_skill      = self.skills.build({name: 'Writting', level: 5})
+    english_understanding_skill = self.skills.build({name: 'Understanding', level: 5})
+
+    english_category.skills << english_speaking_skill
+    english_category.skills << english_writting_skill
+    english_category.skills << english_understanding_skill
+  end
 
 end
