809d0b472a0e2e3c7872992e513b890a9dbe95b9
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-03-06 18:23:51 +0200

    WIP on adapting the profile desing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,5 @@
 module ApplicationHelper
 
-  def profile_picture_for(user, size, action=nil)
-    profile_picture_modifiers = [size]
-    profile_picture_modifiers << 'missing' unless user.avatar.exists?
-
-    content_tag(:div, class: bem_class_for(:'profile-picture', profile_picture_modifiers)) do
-      elements = []
-
-      elements << image_tag(asset_path(user.avatar(size)))
-
-      if action.present? && action[:url].present? && action[:text].present?
-        elements << link_to(action[:text], action[:url], class: "#{bem_class_for(:button, ['primary'])} profile-picture__action")
-      end
-
-      elements.join.html_safe
-    end
-  end
-
   def time_in_ms(time)
     (time.to_f * 1000.0).to_i
   end
