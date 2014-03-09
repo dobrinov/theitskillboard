d8fe3dc8928d217e9df07341eaf42eb53a52295b
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-03-09 20:49:26 +0200

    Add empty demo user page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,18 @@
 class ProfilesController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    if params[:id] == 'demo'
+      @user = User.new(demo_user)
+    else
+      @user = User.find(params[:id])
+    end
+
     @skill_tree = build_skill_tree(@user.skill_categories, @user.skills.order(level: :desc))
+  end
+
+  private
+
+  def demo_user
   end
 
 end
