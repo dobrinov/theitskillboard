500b9f0ba892c59fa077363da89bdc9ae22b82d7
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-03-09 21:55:13 +0200

    Use an existing user for the demo

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,36 +2,11 @@ class ProfilesController < ApplicationController
 
   def show
     if params[:id] == 'demo'
-      @user = demo_user
-    else
-      @user = User.find(params[:id])
+      params[:id] = (1..User.count).to_a.sample
     end
 
+    @user = User.find(params[:id])
     @skill_tree = build_skill_tree(@user.skill_categories, @user.skills.order(level: :desc))
-  end
-
-  private
-
-  def demo_user
-    user = User.new(
-      {
-        name: 'Demo',
-        surname: 'Guy',
-        definition: 'software developer',
-        birth_date: Time.now - 26.years,
-        gender: 'm',
-        country: 'Germany',
-        city: 'Munich',
-        nationality: 'German'
-      }
-    )
-
-    user.interests << Interest.new({name: 'Programming'})
-    user.interests << Interest.new({name: 'Presentations'})
-
-    user.contacts << Contact.new({ctype: :email, address: 'demo@example.com'})
-
-    user
   end
 
 end
